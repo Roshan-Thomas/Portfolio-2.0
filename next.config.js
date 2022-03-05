f2b5448991753e0331d5daf2bf443263e920fda6
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2022-03-05 19:50:42 +0300

    Auto stash before rebase of "refs/heads/main"

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([optimizedImages], { target: 'serverless' });
+// module.exports = withPlugins([optimizedImages], { target: 'serverless' },);
+
+module.exports = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve.fallback.fs = false;
+        }
+        return config;
+      },
+    images: {
+        disableStaticImages: true
+    },
+}
